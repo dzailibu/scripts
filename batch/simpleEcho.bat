ed03bf073c28914bc780b707ab2f22212b680124
dzailibu <fotoknikerboker@gmail.com>
2022-03-18 21:11:35 +0100

    Added simpleEcho batch script

---
+++ batch/simpleEcho.bat
@@ -0,0 +1,13 @@
+@echo off
+echo --------------------------------------
+echo Ovo je neka jednostavna Batch skripta
+echo --------------------------------------
+echo .
+echo .
+echo .
+echo .
+echo .
+echo ------------------------
+echo Kraj izvrsavanja skripte
+echo ------------------------
+pause
