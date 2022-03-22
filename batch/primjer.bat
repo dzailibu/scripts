8abedccd182dac83d83a66f960cacc1f63a93c45
dzailibu <fotoknikerboker@gmail.com>
2022-03-22 15:08:56 +0100

    added two new bat scripts

---
+++ batch/primjer.bat
@@ -0,0 +1,34 @@
+@echo off
+::
+::
+ECHO Molimo vas unestite nesto:
+SET /P var1=
+ECHO Unesena je varijabla: %var1%
+
+PAUSE
+ECHO 'Ovo je Neki Text ZA prikaz'
+ECHO DAtum je: %DATE%
+ECHO Vrijeme je: %DATE%
+ECHO User Profile folder: %USERPROFILE%
+ECHO Pritisnite neki taster na tastaruri da biste potvrdili otvaranje stranice 1
+PAUSE
+start "" https://hubl.biz
+TIMEOUT /t 3
+ECHO Pritisnite neki taster na tastaruri da biste potvrdili otvaranje stranice 5
+PAUSE
+start "" https://google.com
+TIMEOUT /t 3
+ECHO Pritisnite neki taster na tastaruri da biste potvrdili otvaranje stranice 3
+PAUSE
+start "" https://ask.com 
+::
+::
+ECHO da li zelite da zatvorite CMD prozor (y/n)?
+SET /p neto=
+IF %neto%==y (
+	TIMEOUT /t 2
+	EXIT
+	) 
+ELSE ( 
+	ECHO Prozor ostajem otovren 
+	)
