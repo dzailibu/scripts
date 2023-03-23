a30bbd7c545e02ee1c6f636f6c69aba33bf25652
dzailibu <fotoknikerboker@gmail.com>
2023-03-23 14:05:55 +0100

    Added CI-CD folder and batch scripts for build, test and deploy

---
+++ batch/CI-CD/applicationDeployLive.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Application deploy to Live started
+echo git checkout master
+echo git merge review -m "Mergerd test review brench to Master"
+echo Application deploy to Live completed successfully
---
+++ batch/CI-CD/applicationDeployReview.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Application deploy to Review started
+echo git checkout master
+echo git merge NewFeatureBranch -m "Mergerd NewFeatureBranch brench to Master"
+echo Application deploy to Review completed successfully
